951176ad7fa43a060356bc973132d799aab565e0
ArtemNazarov <art_nazarov@mail.ru>
2019-12-20 11:03:43 +0300

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+    <Navbar />
+        
+      </div>
+    </div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700|Work+Sans:300,400,500,600,700');
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container">
+
+    <div class="row">
+        <div class="col s12">
+                <h1>
+        24shop
+    </h1>
+        </div>
+    </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+<style lang="scss" scoped>
+h1 {
+    font-family: 'Poppins', sans-serif;
+}
+.breadcrumbs {
+    padding: 48px 0;
+    margin: 0;
+    color: #a6a6a6;
+    list-style: none;
+    font-family: 'Poppins', sans-serif;
+}
+
+.breadcrumbs li {
+    position: relative;
+    color: #a6a6a6;
+    padding: 0 12px;
+}
+
+.breadcrumbs li:nth-of-type(1) {
+    padding-left: 0;
+}
+
+.breadcrumbs li::after {
+    content: '/';
+    position: absolute;
+    top: 0;
+    right: -6px;
+    width: 10px;
+}
+
+
+.breadcrumbs li:nth-last-of-type(1)::after {
+    display: none;
+}
+
+.breadcrumbs li a {
+    color: #191919;
+    text-decoration: none;
+}
+
+</style>
